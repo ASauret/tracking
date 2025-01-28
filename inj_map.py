c93849aeec8b3a9211075c909c46e17b9f692806
ASauret <[antonin.sauret@esrf.fr]>
2025-01-28 14:47:24 +0100

    Full tracking for injection efficiency map

--- inj_map.py
+++ inj_map.py
@@ -13,45 +13,43 @@ def inj_eff(beam):
     eff = (np.shape(beam0)[1] - lost) / np.shape(beam0)[1]
     return eff * 100, lost
 
-def track_idx(beam,ringpath,endtl2_path):
-    ring = at.load_lattice(ringpath, mat_key='ring')
-    with open(endtl2_path, 'rb') as f1:
-        end_tl2 = pickle.load(f1)
-    idx_nlk2 = end_tl2.get_uint32_index('NLK2')[0]
-    idx_dr2 = end_tl2.get_uint32_index('DR_NLK2')[0]
-    idx_nlk3 = end_tl2.get_uint32_index('NLK3')[0]
-    s_marker = 0.5*end_tl2[idx_nlk2].Length + end_tl2[idx_dr2].Length + end_tl2[idx_nlk3].Length
-    ring.enable_6d()
-    new_ring = ring.sbreak(break_s=s_marker)
-    idx_mk = new_ring.get_uint32_index('sbreak')[0]
-    rot_ring = new_ring.rotate(idx_mk)
-    idx_id04 = rot_ring.get_uint32_index('ID04')[0]
-    return rot_ring.track(beam, nturns=1, refpts=idx_id04)
+lattice_tl2 = at.load_lattice('/machfs/sauret/SharedCodes/tracking/newtl2.mat')
 
-with open('beam_opt2.pickle', 'rb') as f:
-    beam = pickle.load(f)
+twiin = {'beta': [6.73,5.21],
+         'alpha': [-2.21,1.4775],
+         'ClosedOrbit': np.zeros(4),
+         'dispersion': [-0.136, -0.092, 0, 0],
+         'mu': [0, 0]
+         }
 
-beam = np.squeeze(beam[:,:,0,0])
-nturns = 2000
+idx_s3 = lattice_tl2.get_uint32_index('S3')[0]
+_, _, l_opt = lattice_tl2.get_optics(refpts=idx_s3+1, twiss_in=twiin)
+emit_x, emit_y = 30e-9,60e-9
+e_spread, blength = 1.2e-3, 20e-3
+
+end_tl2 = at.load_lattice('/machfs/sauret/SharedCodes/tracking/end_tl2.mat')
+rev_end_tl2 = end_tl2.reverse(copy=True)
+
+orbit_rev = rev_end_tl2.find_orbit(refpts=at.End, orbit=np.array([-8.0e-3,0,0,0,0,0]))
+orbit_rev[1][0][1] *= -1
+# print(orbit_rev)
+
+nparts=10
+opt_beam = at.beam(nparts, at.sigma_matrix(twiss_in=l_opt[0], emity=emit_y, emitx=emit_x, espread=e_spread, blength=blength),
+                   orbit=orbit_rev[1][0])
+
+nturns = 10
 x = np.arange(-2e-3,2.25e-3,0.25e-3)
 xp = np.arange(-2e-3,2.25e-3,0.25e-3)
 xg,xpg = np.meshgrid(x,xp)
-
 all_beams_errors = []
-all_beams_ft = []
 
 for i in range(len(x)):
     for j in range(len(xp)):
-        bi = copy.deepcopy(beam)
+        bi = copy.deepcopy(opt_beam)
         bi[0] += x[i]
         bi[1] += xp[j]
         all_beams_errors.append(bi)
-
-#for k in range(len(all_beams_errors)):
-#    all_beams_ft.append(track_idx(all_beams_errors[k], '/machfs/sauret/SharedCodes/tracking/injection_nlk_mb_qd3.mat', '/machfs/sauret/SharedCodes/tracking/end_tl2.pickle'))
-
-#with open('/machfs/sauret/SharedCodes/tracking/beam_ft.pickle', 'wb') as ft:
-#    pickle.dump(all_beams_ft, ft, pickle.HIGHEST_PROTOCOL)
 
 runner = runners.Injeff_runner_EBS(all_beams_errors, '/machfs/sauret/SharedCodes/tracking/injection_nlk_mb_qd3.mat', 
                                    '/machfs/sauret/SharedCodes/tracking/end_tl2.mat', nturns)
